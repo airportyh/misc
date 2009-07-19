650a8a80e19324ac33cf981000b0b3950598d93d
airportyh <airportyh@Air.local>
2009-07-19 18:29:52 -0400

    separated out jradio.js

---
+++ jRadio/jradio.js
@@ -0,0 +1,28 @@
+(function(){
+  function jRadio(name){
+    this.elements = $('input[type=radio][name=' + name + ']');
+    this.lastVal = jRadio.prototype.val.apply(this);
+  }
+  jRadio.prototype.change = function(callback){
+    var self = this;
+    this.elements.click(function(){
+      var val = self.val();
+      if (val != self.lastVal){
+        self.lastVal = val;
+        callback(self);
+      }
+    })
+  }
+  jRadio.prototype.val = function(val){
+    if (val === undefined){
+      return this.elements.filter(function(){ return this.checked; }).val();
+    }else{
+      this.elements.filter(function(){ return this.val() == val; }).attr('checked', true);
+      return this;
+    }
+  }
+
+  $.radio = function(name){
+    return new jRadio(name);
+  }    
+})();
